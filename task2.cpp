91cb403c8eb81999effd8000ccfa4da45466fa79
just-Tilak <vermatilak364@gmail.com>
2025-06-05 13:18:12 +0530

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;;
+
+int main() {
+    double num1, num2;
+    char ch;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> ch;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch(ch) {
+        case '+':
+            cout << "Result: " << num1 + num2 <<endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 <<endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 <<endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                cout << "Result: " << num1 / num2 <<endl;
+            } else {
+                cout << "Error: Division by zero is not allowed"<<endl;
+            }
+            break;
+        default:
+            cout << "Invalid operator. Please correct."<<endl;
+            break;
+    }
+
+    return 0;
+}
